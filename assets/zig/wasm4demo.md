99e104e7076acda2dcae93a977997ed9a1fc9aed
FabricatorZayac <fed4chuck@gmail.com>
2025-01-30 02:21:04 +0000

    full rewrite in rust

---
+++ assets/zig/wasm4demo.md
@@ -0,0 +1,22 @@
+# Zig wasm4 demo
+
+### What is [wasm4](https://wasm4.org)
+Wasm4 is a fantasy console for making retro-style games with a 4 color screen,
+160x160px framebuffer, memory mapped I/O and a 64kb limit on memory and cart size.
+
+If you have the bindings, you could theoretically use any compiled language to
+write wasm4 games. In my experience, Zig is the most pleasant language for
+doing so.
+
+I've tried Rust before, but while I do enjoy the language, unsafe Rust
+really sucks to work with. Also it's not particularly good for places where every
+byte matters.
+
+### What did I do with it
+
+In it's current state it's a single line which you can move around and
+modify it's angular velocity
+
+[Check it out](https://fabricatorzayac.github.io/zig-wasm4)
+
+[Source code](https://github.com/FabricatorZayac/zig-wasm4)
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::{fs::{self, File}, io::Write, path::Path};
+
+use maud::{html, PreEscaped, DOCTYPE};
+use walkdir::{DirEntry, WalkDir};
+
+fn main() {
+    let asset_paths: Vec<_> = WalkDir::new("assets").into_iter()
+        .filter_map(|e| e.ok())
+        .filter(is_md_file)
+        .map(|e| DirEntry::path(&e).to_owned())
+        .collect();
+
+    println!("{:?}", asset_paths);
+
+    let html_paths: Vec<_> = asset_paths.iter()
+        .map(|mdpath| mdpath.with_extension("html"))
+        .map(|htmlpath| Path::new("site").join(htmlpath.strip_prefix("assets").unwrap()))
+        .collect();
+
+    println!("{:?}", html_paths);
+
+    for (mdpath, htmlpath) in asset_paths.iter().zip(html_paths.iter()) {
+        let html = html! {
+            (DOCTYPE)
+            head {
+                link rel="stylesheet" href="/style.css";
+            }
+            body {
+                button class="button-54" type="button" onclick="toggle()" { "Toggle theme" }
+                (parse_markdown(&fs::read_to_string(mdpath).unwrap()))
+                script src="/script.js" {}
+                script { "history.replaceState({page: 1}, '', window.location.pathname.replace('.html', ''))" }
+            }
+        };
+        fs::create_dir_all(htmlpath.parent().unwrap()).unwrap();
+        let mut file = File::create(htmlpath).unwrap();
+        file.write_all(html.into_string().as_bytes()).unwrap();
+    }
+}
+
+fn is_md_file(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.ends_with(".md"))
+        .unwrap_or(false)
+}
+
+fn parse_markdown(md: &str) -> PreEscaped<String> {
+    let mut options = comrak::Options::default();
+    options.extension.underline = true;
+    options.extension.strikethrough = true;
+
+    let adapter = comrak::plugins::syntect::SyntectAdapterBuilder::new()
+        .theme("InspiredGitHub")
+        .build();
+
+    let mut plugins = comrak::Plugins::default();
+    plugins.render.codefence_syntax_highlighter = Some(&adapter);
+
+    PreEscaped(comrak::markdown_to_html_with_plugins(md, &options, &plugins))
+}
